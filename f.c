eb6251b24e68de16c38558325d77c29c6a23724f
Will Eccles <will@eccles.dev>
2021-01-20 11:07:42 -0500

    add readme and format

--- f.c
+++ f.c
@@ -8,53 +8,53 @@
 #include <libgen.h>
 
 void printitem(char* name, char* val) {
-    if (name && *name && val && *val) {
-        printf("\033[36m%7s\033[m | %s\n", name, val);
-    }
+  if (name && *name && val && *val) {
+    printf("\033[36m%7s\033[m | %s\n", name, val);
+  }
 }
 
 int main(void) {
-    struct utsname un;
-    if (uname(&un) == -1) {
-        perror("error getting system info");
-        return 1;
-    }
+  struct utsname un;
+  if (uname(&un) == -1) {
+    perror("error getting system info");
+    return 1;
+  }
 
-    struct passwd* pw = getpwuid(getuid());
-    if (!pw) {
-        perror("error getting user info");
-        return 1;
-    }
+  struct passwd* pw = getpwuid(getuid());
+  if (!pw) {
+    perror("error getting user info");
+    return 1;
+  }
 
-    char* editor = getenv("EDITOR");
-    if (editor && *editor) editor = basename(editor);
+  char* editor = getenv("EDITOR");
+  if (editor && *editor) editor = basename(editor);
 
-    printf(" \033[32m%s\033[m@\033[32m%s\033m\n",
-            pw->pw_name, un.nodename);
+  printf(" \033[32m%s\033[m@\033[32m%s\033m\n",
+         pw->pw_name, un.nodename);
 
-    char* os = malloc(strlen(un.sysname) + strlen(un.release) + 2);
-    if (!os) {
-        perror("malloc");
-        return 1;
-    }
+  char* os = malloc(strlen(un.sysname) + strlen(un.release) + 2);
+  if (!os) {
+    perror("malloc");
+    return 1;
+  }
 
-    sprintf(os, "%s %s", un.sysname, un.release);
+  sprintf(os, "%s %s", un.sysname, un.release);
 
-    printitem("os", os);
-    printitem("arch", un.machine);
-    printitem("editor", editor);
-    printitem("shell", basename(pw->pw_shell));
+  printitem("os", os);
+  printitem("arch", un.machine);
+  printitem("editor", editor);
+  printitem("shell", basename(pw->pw_shell));
 
-    for (int i = 0; i < 6; i++) {
-        printf(" \033[%dm▄▄\033[m", 31 + i);
-    }
-    printf("\n");
-    for (int i = 0; i < 6; i++) {
-        printf(" \033[%dm▀▀\033[m", 91 + i);
-    }
-    printf("\n");
+  for (int i = 0; i < 6; i++) {
+    printf(" \033[%dm▄▄\033[m", 31 + i);
+  }
+  printf("\n");
+  for (int i = 0; i < 6; i++) {
+    printf(" \033[%dm▀▀\033[m", 91 + i);
+  }
+  printf("\n");
 
-    free(os);
+  free(os);
 
-    return 0;
+  return 0;
 }
